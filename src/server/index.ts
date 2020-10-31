17e324378b1bb2c6d130253798a69cced22dc963
Luciano Serruya Aloisi <lucianoserruya@gmail.com>
2020-10-31 16:40:44 -0300

    Implement full-text search via Elasticsearch

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import { Client } from "@elastic/elasticsearch";
 import { firestore, storage } from "firebase-admin";
 import { logger } from "firebase-functions";
 
@@ -18,7 +19,7 @@ export const createStaticFileApp = () => {
   return app;
 };
 
-export const createRestApi = (prefix: string = "/api/contacts") => {
+export const createRestApi = (prefix: string = "/contacts") => {
   interface ContactData {
     firstName?: string;
     lastName?: string;
@@ -30,6 +31,9 @@ export const createRestApi = (prefix: string = "/api/contacts") => {
 
   const CONTACTS_COLLECTION_NAME = "contacts";
   const contactsCollection = db.collection(CONTACTS_COLLECTION_NAME);
+
+  const esClient = new Client({ node: "http://localhost:9200" });
+  const ES_INDEX_NAME = "contacts";
 
   app.use(express.json());
 
@@ -44,11 +48,43 @@ export const createRestApi = (prefix: string = "/api/contacts") => {
   app
     .route(prefix)
     .get(async (req, res) => {
-      const { docs } = await contactsCollection.get();
-      const contacts = docs.map(doc => {
-        return { id: doc.id, ...doc.data() };
-      });
-      return res.json({ data: { contacts } });
+      const { q } = req.query;
+      if (q) {
+        const { body } = await esClient.search({
+          index: ES_INDEX_NAME,
+          body: {
+            query: {
+              bool: {
+                should: [
+                  { wildcard: { firstName: `*${q}*` } },
+                  { wildcard: { lastName: `*${q}*` } },
+                  { wildcard: { address: `*${q}*` } }
+                ]
+              }
+            }
+          }
+        });
+        const contactIds: string[] = body.hits.hits.map(
+          (hit: { _id: string }) => hit._id
+        );
+        const contacts = await Promise.all(
+          contactIds.map(async id => {
+            const doc = await contactsCollection.doc(id).get();
+            if (!doc.exists) return;
+            return { id: doc.id, ...doc.data() };
+          })
+        );
+
+        return res.json({
+          data: { contacts: contacts.filter(contact => contact) }
+        });
+      } else {
+        const { docs } = await contactsCollection.get();
+        const contacts = docs.map(doc => {
+          return { id: doc.id, ...doc.data() };
+        });
+        return res.json({ data: { contacts } });
+      }
     })
     .get(async (req, res) => {
       const { id } = req.params;
@@ -70,6 +106,16 @@ export const createRestApi = (prefix: string = "/api/contacts") => {
         return res.status(400).end();
       }
       const doc = await contactsCollection.add(data);
+
+      await esClient.index({
+        index: ES_INDEX_NAME,
+        id: doc.id,
+        body: {
+          firstName,
+          lastName,
+          address
+        }
+      });
 
       return res.status(201).json({
         status: "SUCCESS",
@@ -94,6 +140,16 @@ export const createRestApi = (prefix: string = "/api/contacts") => {
 
       await contactsCollection.doc(id).set(data);
 
+      await esClient.index({
+        index: ES_INDEX_NAME,
+        id: doc.id,
+        body: {
+          firstName,
+          lastName,
+          address
+        }
+      });
+
       const contact = { id: doc.id, ...data };
       return res.json({ data: contact });
     })
@@ -107,6 +163,8 @@ export const createRestApi = (prefix: string = "/api/contacts") => {
       }
 
       await contactsCollection.doc(id).delete();
+
+      await esClient.delete({ index: ES_INDEX_NAME, id });
 
       return res.status(204).end();
     });
